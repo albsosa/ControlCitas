3dea9b73ac1fc164e0e0a16c780e5d2e615640bb
albsosa <inf.albsosa@gmail.com>
2019-09-06 17:51:44 -0500

    Inicio de los tiempos

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1 className="text-center">Administrador de Pacientes de Veterinaria</h1>
+      </header>
+      <div className="row mt-3">
+        <div className="col-md-6">
+          Formulario aquí
+        </div>
+        <div className="col-md-6">
+          Listado Aquí
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
